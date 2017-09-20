86501b3a2712c758229ee04a8fa43ca0a9bcc8ca
hurrialice <zhangq07@yahoo.com>
2017-09-20 18:00:38 +0800

    for quantify expression from stringtie gene abundancy tables for quantify methylation (both IP and input) from stringtie gtfs

---
+++ Rscripts/quant/exp-quant.R
@@ -0,0 +1,47 @@
+library(readr)
+library(dplyr)
+library(tibble)
+gene_pool <- read_rds('sorted_genes.rds')
+
+
+
+
+uni_tab <- function(t){
+    t <- t %>% dplyr::rename(geneName = `Gene Name`) %>% dplyr::select(geneName, FPKM)
+    rep.genes <- unique(t$geneName[duplicated(t$geneName)])
+    t_single <- t %>% dplyr::filter(!geneName %in% rep.genes)
+    t_multi <- t %>% dplyr::filter(geneName %in% rep.genes)
+    tl <- lapply(split(t_multi, t_multi$geneName), function(df){
+        geneName <- unique(df$geneName)
+        FPKM <- sum(df$FPKM)
+        tibble(geneName,FPKM)
+    })
+    o <- rbind(do.call(rbind, tl), t_single)
+    o[order(o$geneName),]
+}
+
+d <- read_rds('srr_withgtfs.rds')
+d <- d %>% dplyr::filter(grepl("_input", wecall))
+home_path <- "/home/qingzhang/meth-qing/stringtie-exps-merged1/abun/"
+file_names <- paste0(d$wecall, ".tab")
+
+
+#create a data container
+mc <- matrix(nrow = nrow(d), ncol = length(gene_pool), 
+             dimnames = list(d$sra_acc, gene_pool))
+
+
+for (i in seq_along(file_names)){
+    print(i)
+    file_to_read <- paste0(home_path, file_names[i])
+    srr_id <- d$sra_acc[i]
+    
+    t0 <- read_tsv(file_to_read)
+    t1 <- uni_tab(t0)
+    
+    mc[srr_id,] <- t1$FPKM[match(colnames(mc), t1$geneName)]
+}
+
+write_rds(mc, 'test_exp_raw.rds')
+
+
---
+++ Rscripts/quant/meth-quant2.R
@@ -0,0 +1,44 @@
+library(rtracklayer)
+library(readr)
+library(dplyr)
+library(tibble)
+library(GenomicFeatures)
+library(GenomicRanges)
+
+rm <- read_table2('RMBase_hg19_all_m6A_site.txt')
+rm0 <- rm %>% filter(!is.na(score2)) %>% filter(supportNum > 10) %>%
+    dplyr::select(chromosome, modStart, modEnd, modName, strand, 
+                  supportNum, pubmedIds, geneName, geneType) %>% 
+    dplyr::rename(transcript_id = modName, gene_id = geneName, 
+           start = modStart, end = modEnd, seqname = chromosome)
+rm0$type <- 'exon'
+rmdf <- DataFrame(rm0)
+rmgr <- makeGRangesFromDataFrame(rmdf, keep.extra.columns = TRUE)
+rm(rm, rm0)
+
+# read FPKM values.
+d <- read_rds('srr_withgtfs.rds')
+msites <- read_rds('msites.rds')
+home_path <- "/home/qingzhang/meth-qing/stringtie-meths/"
+
+gtf2df <- function(file_to_read){
+    gr <- rtracklayer::import(file_to_read, 'gtf')
+    df <- as.tibble(mcols(gr)) %>% dplyr::filter(type == 'transcript') %>% 
+        dplyr::select( transcript_id, FPKM) %>% dplyr::rename(modName = transcript_id)
+}
+
+
+# initialize with a container
+mc <- matrix(nrow = nrow(d), ncol = length(msites), 
+                   dimnames = list(d$sra_acc, msites))
+for (i in seq(nrow(d))){
+    print(i)
+    srr_id <- d$sra_acc[i]
+    file_to_read <- paste0(home_path, d$wecall[i], '.gtf')
+    df <- gtf2df(file_to_read)
+    print(df)
+    mc[srr_id,] <- df$FPKM[match(colnames(mc), df$modName)]
+}
+
+write_rds(mc, 'test_m_raw.rds')
+
